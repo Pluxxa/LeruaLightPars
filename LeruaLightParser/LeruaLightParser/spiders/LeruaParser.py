244e93794f8442457d240da46466d8ae391f3537
Pluxxa <95534978+Pluxxa@users.noreply.github.com>
2024-06-01 04:19:29 +0300

    add code

--- LeruaLightParser/LeruaLightParser/spiders/LeruaParser.py
+++ LeruaLightParser/LeruaLightParser/spiders/LeruaParser.py
@@ -1,27 +1,50 @@
 import scrapy
 
 
-class LeruaparserSpider(scrapy.Spider):
-    name = "LeruaParser"
-    allowed_domains = ["https://www.divan.ru/"]
+
+class LParsSpider(scrapy.Spider):
+    name = "l_pars"
+    allowed_domains = ["divan.ru"]
     start_urls = ["https://www.divan.ru/category/svet/page-1"]
+    custom_settings = {
+        'FEEDS': {
+            'output.json': {
+                'format': 'json',
+                'encoding': 'utf8',
+                'store_empty': False,
+                'fields': None,
+                'indent': 4,
+            },
+        }
+    }
 
     def parse(self, response):
         lights = []
         try:
-            for i in range(100):
-                url = f"https://www.divan.ru/category/svet/page-{i+1}"
-                light = response.css('div.lsooF')
-                lights.append(light)
+            links = []
+            link = response.css('div.ui-jDl24')
+            for i in link.css('a::attr(href)'):
+                if len(links) == 0:
+                    links.append(i)
+                else:
+                    flag = True
+                    for j in links:
+                        if i == j:
+                            flag = False
+                        else:
+                            continue
+                    if flag:
+                        links.append(i)
+                    else:
+                        continue
+            l = links[:-1]
+            print(*links, sep='\n')
+            print()
+            print(*l, sep='\n')
         except IndexError:
             return 0
-        a = []
-        for li in lights:
-            for l in li:
-                a.append(l)
-                yield {
-                    "name" : l.css('div.lsooF span::text').get(),
-                    "price" : l.css('div.q5Uds span::text').get(),
-                    "url" : l.css('a').attrib['href']
-                }
-        print(len(a))
+
+
+    #next_page = response.css('a.next::attr(href)').get()
+
+
